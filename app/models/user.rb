8024a8c754a9c510477b11fe80650517efe4226c
Alex N <alex@vinova.sg>
2011-05-14 17:18:24 +0800

    Add fields and indexes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User
   # :lockable, :timeoutable and :omniauthable, :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # Twitter
+  field :twitter_handle, :type => String
+  field :twitter_oauth_token, :type => String
+  field :twitter_oauth_secret, :type => String
+  
+  index :twitter_handle, :unique => true
+  index [[:twitter_oauth_token, 1], [:twitter_oauth_secret, 1]]
+  
+  include Mongoid::Likeable
 end
