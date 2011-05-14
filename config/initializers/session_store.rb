7211ce77c023e60ca618b2cb4e56ef573160319a
Alex N <alex@vinova.sg>
2011-05-15 00:19:04 +0800

    Refine

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Shdhsg::Application.config.session_store :cookie_store, :key => '_namecard_session'
+Shdhsg::Application.config.session_store :cookie_store, :key => '_awesomeness_shdhsg_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
