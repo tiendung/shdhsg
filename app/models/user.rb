8f7a5f243ba63ddc278aea9dfb7d6435c3dcb2ae
Alex N <alex@vinova.sg>
2011-05-15 01:59:55 +0800

    Really save twitter_info

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,30 @@ class User
   
   before_save :update_info_via_twitter
 
+  def self.create_lord!
+    lord = User.new
+    lord.twitter_id = 'shdhsg'
+    lord.credit = 999_999_999
+    lord.save(:validate => false)
+    lord
+  end
+  
+  def self.lord
+    @@lord = User.where(:twitter_id => 'shdhsg').first || create_lord!
+  end
+  
+  def lord?
+    self == User.lord
+  end
+  
+  after_create do
+    User.lord.like(self, 'you attend SuperHappyDevHouseSG')
+  end
+
   def update_info_via_twitter(force = false)
     if force || twitter_id_changed?
       t = Twitter::Client.new.user(twitter_id)
-      twitter_info = t.to_hash
+      self.twitter_info = t.to_hash
       self.avatar_url = t.profile_image_url
     end
   end
